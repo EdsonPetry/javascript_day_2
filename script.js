2aadd976e569cf3c88c64449831cb8855f48e540
Edson Petry <edsonlbpetry@gmail.com>
2023-03-09 14:30:59 -0500

    end of class

---
+++ script.js
@@ -0,0 +1,17 @@
+var age = 10
+var myName = 'Ed'
+
+var operator = '-'
+
+var a = 10
+var b = 15
+
+if(age) {
+    console.log('Is old enough')
+} else if (age === 18) {
+    console.log('Old enough to party')
+} else if (age < 18 && age > 12) {
+    console.log('You are a brat.')
+} else {
+    console.log('Not old enough!')
+}
